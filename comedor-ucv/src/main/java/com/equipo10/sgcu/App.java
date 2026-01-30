ddeed1378a91cf8782a719b819f49623f2bb61dc
Daniel Moros <danielito.moros03@gmail.com>
2026-01-30 13:54:02 -0400

    Primeros Directorios Creados con Maven

---
+++ comedor-ucv/src/main/java/com/equipo10/sgcu/App.java
@@ -0,0 +1,13 @@
+package com.equipo10.sgcu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
