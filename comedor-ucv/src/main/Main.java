08e0747e36f295e51e3ca9200861e232146a0074
Andre <yoseannibt305@gmail.com>
2026-02-06 14:44:11 -0400

    Ignoren, experimento

--- comedor-ucv/src/main/Main.java
+++ comedor-ucv/src/main/Main.java
@@ -2,7 +2,6 @@ package PracticaJava.Inge;
 import javax.swing.SwingUtilities;
 
 import PracticaJava.Inge.Vista.BienvenidoVista;
-//import PracticaJava.Inge.Vista.Login;
 import PracticaJava.Inge.Controlador.LoginControl;
 
 
@@ -11,7 +10,6 @@ public class Main {
         public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             new LoginControl();
-            new BienvenidoVista().setVisible(true);
         });
     }
 }
