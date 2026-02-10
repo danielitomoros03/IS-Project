5d5a4878bfe987312c6f24efa2dddf121a0f247c
Andre <yoseannibt305@gmail.com>
2026-02-09 22:26:25 -0400

    Limpieza

--- comedor/src/main/java/com/example/Main.java
+++ comedor/src/main/java/com/example/Main.java
@@ -1,9 +1,8 @@
 package com.example;
 
-import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-
 import com.example.Controlador.*;
+import com.example.Modelo.RegUsuarioModelo;
 import com.example.Vista.*;
 
 
@@ -12,13 +11,19 @@ public static void main(String[] args) {
     SwingUtilities.invokeLater(() -> {
         //Esto es para abrir la ventana de Admin, ya que aun no se ha realizado la 
         //la validacion para cuando sea admin, pueda entrar a esta interfaz
-        System.out.println("Intentando iniciar AdminControl..."); // PASO 1
-        try {
-            AdminControl admin = new AdminControl("Administrador_UCV", "Admin");
+        
+        //System.out.println("Intentando iniciar AdminControl..."); // PASO 1
+        RegistroVista reg = new RegistroVista();
+        RegUsuarioModelo regC = new RegUsuarioModelo();
+/*        try {
+            RegistroControl admin = new RegistroControl(reg, regC);
             System.out.println("AdminControl iniciado con éxito."); // PASO 2
         } catch (Exception e) {
             e.printStackTrace(); // Esto te dirá el error exacto en la consola roja
-        }
+        }  */
+
+            //RegistroControl bista = new RegistroControl(reg, regC);
+            
 
         //Interza de inicio de sesion
         LoginControl login = new LoginControl();
